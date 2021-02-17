10b92593a8158171fa5b2d200f9d1e33cfdab980
Ryan Aruanno <ryan.aruanno.18@cnu.edu>
2021-02-16 19:15:56 -0500

    done

---
+++ fileio/fileio.cpp
@@ -0,0 +1,16 @@
+#include "../includes/constants.h"
+#include "../includes/fileio.h"
+using namespace std;
+
+
+//attempt to open file 'filename' and read in all data
+//returns SUCCESS if all goes well or COULD_NOT_OPEN_FILE
+int loadData(const std::string filename, vector<process> &myProcesses){
+	return UNIMPLEMENTED;
+}
+
+//attempt to create or open file 'filename' to write all data to
+//returns SUCCESS if all goes well or COULD_NOT_OPEN_FILE
+int saveData(const std::string filename, vector<process> &myProcesses){
+	return UNIMPLEMENTED;
+}
---
+++ includes/fileio.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "../includes/constants.h"
+
+//attempt to open file 'filename' and read in all data
+//returns SUCCESS if all goes well or COULD_NOT_OPEN_FILE
+int loadData(const std::string filename, std::vector<process> &myProcesses);
+
+//attempt to create or open file 'filename' to write all data to
+//returns SUCCESS if all goes well or COULD_NOT_OPEN_FILE
+int saveData(const std::string filename, std::vector<process> &myProcesses);
+
